f43791f64a2213bc6dd3a0df2cc45e607696cf0f
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-03-01 11:59:03 -0300

    test back vercel2

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -14,7 +14,7 @@ import axios from 'axios'
 
 export function getAllRecipes(){
      return async function(dispatch){
-        const re = await axios.get('http://localhost:3001/recipes')
+        const re = await axios.get('https://pi-back-deploy.vercel.app/')
           return dispatch({
               type: GET_ALL_RECIPES,
               payload: re.data
@@ -25,7 +25,7 @@ export function getAllRecipes(){
 
 export function getAllDiets(){
      return async function(dispatch){
-        const re = await axios('http://localhost:3001/diets')
+        const re = await axios('https://pi-back-deploy.vercel.app/')
          dispatch({
             type: GET_ALL_DIETS,
             payload: re.data
@@ -36,7 +36,7 @@ export function getAllDiets(){
 
 export function getRecipesById(id){
       return async function(dispatch){
-        const re = await axios(`http://localhost:3001/recipes/${id}`)
+        const re = await axios(`https://pi-back-deploy.vercel.app/${id}`)
         dispatch({
             type: GET_RECIPES_ID,
             payload: re.data
@@ -46,7 +46,7 @@ export function getRecipesById(id){
 
 export function getRecipesByName(name){
      return async function(dispatch){
-        const re = await axios(`http://localhost:3001/recipes?name=${name}`)
+        const re = await axios(`https://pi-back-deploy.vercel.app/recipes?name=${name}`)
         dispatch({
              type: GET_RECIPES_NAME,
              payload: re.data
